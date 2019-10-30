82a045a4d53b4a5ea5174cb6c91ff04c3d187652
Lena <2863040@ukr.net>
2019-10-30 13:36:57 +0200

    Updates

---
+++ precache-manifest.9cc6db8b74875d036c03807ea25cdfad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34831fdfe83e5c848836dbde7c9b86fc",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "5ba3a81c160acd4d12c2",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "d5512a6c2374a92aacf7",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.0ae04533.chunk.js"
+  },
+  {
+    "revision": "5ba3a81c160acd4d12c2",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.3aadd30c.chunk.js"
+  },
+  {
+    "revision": "dffe8cd1e2fc699743fb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.d2e8316a.js"
+  }
+]);
